40435f59a470aa3ec5556c229c48848671f6289e
m.abdelwahab <m.abdelwahab@tbc-me.com>
2021-05-18 21:48:36 +0200

    1

---
+++ WebApplication4/Controllers/VisionsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GenieMistro.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace GenieMistro.Controllers
+{
+    [Route("~/api/[controller]/[action]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class VisionsController : ControllerBase
+    {
+        private readonly genieDBContext _context;
+
+        public VisionsController(genieDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Visions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vision>>> GetVisions()
+        {
+            return await _context.Visions.ToListAsync();
+        }
+
+        // GET: api/Visions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vision>> GetVision(int id)
+        {
+            var vision = await _context.Visions.FindAsync(id);
+
+            if (vision == null)
+            {
+                return NotFound();
+            }
+
+            return vision;
+        }
+
+        // PUT: api/Visions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVision(int id, Vision vision)
+        {
+            if (id != vision.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vision).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VisionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Visions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Vision>> PostVision(Vision vision)
+        {
+            _context.Visions.Add(vision);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVision", new { id = vision.Id }, vision);
+        }
+
+        // DELETE: api/Visions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVision(int id)
+        {
+            var vision = await _context.Visions.FindAsync(id);
+            if (vision == null)
+            {
+                return NotFound();
+            }
+
+            _context.Visions.Remove(vision);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VisionExists(int id)
+        {
+            return _context.Visions.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApplication4/Services/landLoard.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web.Http.ModelBinding;
+
+namespace GenieMistro.Services
+{
+    public class landLoard
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Domain { get; set; }
+
+        public String GetDatabase(String Email)
+        {
+            landLoard landLoardDB = null;
+            MailAddress address = new MailAddress(Email);
+            string host = address.Host;
+            IEnumerable<landLoard> Dbs = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:64189/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("GetTbLandLoards");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<landLoard>>();
+                    readTask.Wait();
+
+                    Dbs = readTask.Result;
+                }
+               
+                foreach (landLoard item in Dbs)
+                {
+                    if (item.Domain == host)
+                    {
+                        landLoardDB = item;
+                        break;
+                    }
+                }
+
+            }
+            
+            return landLoardDB.Name;
+        }
+
+    }
+}
